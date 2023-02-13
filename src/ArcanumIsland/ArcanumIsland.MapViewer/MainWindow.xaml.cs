c3de450978d962cf84b770a514164e17ddf34d3d
Alex <AlexKozlovDeveloper@gmail.com>
2023-02-13 14:07:49 +0300

    Update tp net7.0

--- src/ArcanumIsland/ArcanumIsland.MapViewer/MainWindow.xaml.cs
+++ src/ArcanumIsland/ArcanumIsland.MapViewer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PerlinNoise;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,47 @@ namespace ArcanumIsland.MapViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random _rnd;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _rnd = new Random(696);
         }
-    }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            var generator = new PerlinNoiseGenerator(_rnd.Next(), 255);
+
+            var resX = 256;
+            var resY = resX;
+
+            var matrix = generator.GetPerlinNoiseMatrix(resX, 4);
+
+            WriteableBitmap writableImg = new WriteableBitmap(resX, resY, 96, 96, PixelFormats.Bgr32, null);
+
+            //lock the buffer
+            writableImg.Lock();
+
+            for (int i = 0; i < resX; i++)
+            {
+                for (int j = 0; j < resY; j++)
+                {
+                    IntPtr backbuffer = writableImg.BackBuffer;
+                    //the buffer is a monodimensionnal array...
+                    backbuffer += j * writableImg.BackBufferStride;
+                    backbuffer += i * 4;
+                    System.Runtime.InteropServices.Marshal.WriteInt32(backbuffer, matrix[i][j]);
+                }
+            }
+
+            //specify the area to update
+            writableImg.AddDirtyRect(new Int32Rect(0, 0, resX, resY));
+            //release the buffer and show the image
+            writableImg.Unlock();
+
+            MainImage.Source = writableImg;
+        }
+    }   
 }
